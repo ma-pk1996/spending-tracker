e93ed5620c37109973096cbc84c42670154d31ea
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2023-08-08 20:09:50 +0330

    orgnizing the files

---
+++ src/Components/Expense-comp/ExpenseCards.js
@@ -0,0 +1,42 @@
+import './ExpenseCards.css';
+import ExpenseDate from '../Common-comp/ExpenseDate';
+import Card from '../Common-comp/Card';
+
+
+function ExpenseCard (props) {
+
+    return (
+        <Card className="ExpenseCard">
+            <ExpenseDate date={props.date} />
+            <div className="ExpenseCard_Description">
+                <h6>{props.title}</h6>
+                <div className="ExpenseCard__Price">${props.amount}</div>      
+            </div>
+        </Card>
+    );
+}
+
+export default ExpenseCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
