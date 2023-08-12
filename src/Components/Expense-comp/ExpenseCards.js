9bc5df80a36b7c333eed5f8ace187baea63d9002
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2023-08-12 20:36:07 +0330

    using "usestate" hook to re-render the app root when we need to change th DOM

--- src/Components/Expense-comp/ExpenseCards.js
+++ src/Components/Expense-comp/ExpenseCards.js
@@ -1,17 +1,22 @@
+import React, { useState } from 'react';
 import './ExpenseCards.css';
 import ExpenseDate from '../Common-comp/ExpenseDate';
 import Card from '../Common-comp/Card';
 
 
 function ExpenseCard (props) {
-
+    const [title, settitle] = useState(props.title);
+    const clickHandler = () => {
+        settitle('updated!');
+    }
     return (
         <Card className="ExpenseCard">
             <ExpenseDate date={props.date} />
             <div className="ExpenseCard_Description">
-                <h6>{props.title}</h6>
+                <h6>{title}</h6>
                 <div className="ExpenseCard__Price">${props.amount}</div>      
             </div>
+            <button onClick={clickHandler}>click me</button>
         </Card>
     );
 }
