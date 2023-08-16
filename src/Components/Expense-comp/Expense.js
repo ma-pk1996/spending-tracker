1201cfaed5f374762ee6d09ffb47da14446fb36f
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2023-08-16 21:12:48 +0330

    how to get props dynamically from parent component

--- src/Components/Expense-comp/Expense.js
+++ src/Components/Expense-comp/Expense.js
@@ -6,21 +6,11 @@ import Card from "../Common-comp/Card";
 function Expense(props) {
     return (
         <Card className="expenses">
-        <ExpenseCard
-          title={props.items[0].title}
-          date={props.items[0].date}
-          amount={props.items[0].amount}
-        />
-        <ExpenseCard
-          title={props.items[1].title}
-          date={props.items[1].date}
-          amount={props.items[1].amount}
-        />
-        <ExpenseCard
-          title={props.items[2].title}
-          date={props.items[2].date}
-          amount={props.items[2].amount}
-        />
+        {props.items.map((expense) => <ExpenseCard
+          title={props.title}
+          amount={props.amount}
+          date={props.date}
+        />)}
         </Card>
     );
 }
