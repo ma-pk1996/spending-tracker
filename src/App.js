d8bc543852a85615d5d44790f1725b19b1ca0c4b
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2023-08-18 13:03:38 +0330

    adding new data to our database

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, {useState} from 'react';
 import Expense from './Components/Expense-comp/Expense.js';
 import NewExpense from "../../spending-tracker/src/Components/Expense-comp/NewExpense.jsx";
 
-const Expenses = [
+const FakeExpenses = [
   {id: 'e1', title: 'car rent', amount: 200, date: new Date(2023,7,13)},
   {id: 'e2', title: 'car wash', amount: 200, date: new Date(2023,7,13)},
   {id: 'e3', title: 'car repair', amount: 200, date: new Date(2023,7,13)}
 ];
 
 function App() {
+  const [expenses, setExpense] = useState(FakeExpenses);
   
-  
-  function setAddExpense(recieveddata) {
-    
+  function setAddExpense(expense) {
+      setExpense((prevExpenses) => {
+        return [expense, ...prevExpenses];
+      })
   }
   return (
     <div>
       <NewExpense  onAddExpense={setAddExpense}/>
-      <Expense items={Expenses} />
+      <Expense items={expenses} />
     </div>
   );
 }
